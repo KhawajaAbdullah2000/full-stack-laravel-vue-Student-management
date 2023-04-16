713bf6f131a9abd981a0f24c38704394088f6aca
unknown <k200385@nu.edu.pk>
2023-04-16 13:53:03 +0500

    Crud completed for apis

--- crudrestapi/app/Http/Controllers/StudentController.php
+++ crudrestapi/app/Http/Controllers/StudentController.php
@@ -60,4 +60,83 @@ class StudentController extends Controller
             }
 
     }
+
+    public function show($id){
+        $student=Student::find($id);
+        if($student){
+            return response()->json([
+                'status'=>200,
+                'student'=>$student
+            ],200);
+        }
+        else{
+            return response()->json([
+                'status'=>404,
+                'message'=>'Student not found'
+            ],404);
+        }
+    }
+
+    public function edit($id){
+        $student=Student::find($id);
+        if($student){
+            return response()->json([
+                'status'=>200,
+                'student'=>$student
+            ],200);
+        }
+        else{
+            return response()->json([
+                'status'=>404,
+                'message'=>'Student not found'
+            ],404);
+        }
+    }
+
+    public function update(Request $req,int $id){
+        $validator=Validator::make($req->all(),
+        [
+           'name'=>'required|string|max:20',
+           'course'=>'required|string|max:20',
+           'email'=>'required|email|unique:students|max:30',
+        ]);
+            if ($validator->fails()){
+                return response()->json([
+                    'status'=>422,
+                    'errors'=>$validator->messages()
+                ],422);
+            }
+            $student=Student::find($id);
+            if ($student){
+                $student->update([
+                    'name'=>$req->name,
+                    'course'=>$req->course,
+                    'email'=>$req->email,
+                ]);
+                return response()->json([
+                    'status'=>200,
+                    'message'=>'Student updated successfully'
+                ],200);
+
+            }
+                else{
+                    return response()->json([
+                        'status'=>404,
+                        'message'=>'No such student found'
+                    ],404);
+                }
+    }
+
+    public function destroy($id){
+        $student=Student::find($id);
+        if ($student){
+            $student->delete();
+            
+        }else{
+            return response()->json([
+                'status'=>404,
+                'message'=>'No such student found'
+            ],404);
+        }
+    }
 }
